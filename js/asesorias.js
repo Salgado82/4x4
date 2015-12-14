a5144dbd5d1efeb364fb8e72d9808b60dc372946
Carlos Salgado <carlosalgado.82@gmail.com>
2015-12-14 06:28:32 -0600

    Pagina actual

--- js/asesorias.js
+++ js/asesorias.js
@@ -1,7 +1,11 @@
 function getGold()
 {
 	var txt = "";
-	
+							
+	txt +="<div id='lista'>";
+	txt +="<div class='col-lg-2 col-md-2 flechas-asesorias-izq'><div class='flechas'><a onclick='getSilver();return false;' href='#''><h1><<</h1></a></div></div>";
+	txt +="<div class='col-lg-8 col-md-8 col-sm-12 col-xs-12'>";
+	txt +="<ul style='padding:0;text-align:center;font-family:noteworthy-light' class='good'>";
 	txt +="<li><h3>ASESORÍA GOLD</h3></li>";
 	txt +="<li>&nbsp;</li>";
 	txt +="<li>Planteamiento de objetivos de Cliente</li>";
@@ -14,9 +18,15 @@ function getGold()
 	txt +="<li>Propuestas de cambio de look de cabello</li>";
 	txt +="<li>Propuestas de maquillaje</li>";
 	txt +="<li>Propuestas de looks de inspiración</li>";
-	txt +="<li>Closet Fix</li>";
+	txt +="<li>Closet Fix o armado de looks en closet</li>";
 	txt +="<li>Básicos de Armario</li>";
+	txt +="<li>Incluye: carpeta de diagnóstico y paleta de color de la estación correspondiente.</li>";
 	txt +="<li>&nbsp;</li>";
+	txt +="</ul>";
+	txt +="</div>";
+	txt +="<div class='col-lg-2 col-md-2 flechas-asesorias-der'>";
+	txt +="<div class='flechas'><a onclick='getPlatinum();return false;' href='#''><h1>>></h1></a></div>";
+	txt +="</div></div>";
 
 	$("#lista").html(txt);
 	$("#lista").hide();
@@ -27,6 +37,10 @@ function getSilver()
 {
 	var txt = "";
 	
+	txt +="<div id='lista'>";
+	txt +="<div class='col-lg-2 col-md-2 flechas-asesorias-izq'></div>";
+	txt +="<div class='col-lg-8 col-md-8 col-sm-12 col-xs-12'>";
+	txt +="<ul style='padding:0;text-align:center;font-family:noteworthy-light' class='good'>";
 	txt +="<li><h3>ASESORÍA SILVER</h3></li>";
 	txt +="<li>&nbsp;</li>";
 	txt +="<li>&nbsp;</li>";
@@ -38,10 +52,15 @@ function getSilver()
 	txt +="<li>Anàlisis de color y drapeado.</li>";
 	txt +="<li>Recomendaciones de prendas y accesorios.</li>";
 	txt +="<li>Propuestas de looks de inspiración</li>";
-	txt +="<li>Incluye: carpetas de diagnóstico y paleta de colores de la estación correspondiente.</li>";
+	txt +="<li>Incluye: carpeta de diagnóstico y paleta de color de la estación correspondiente.</li>";
 	txt +="<li>&nbsp;</li>";
 	txt +="<li>&nbsp;</li>";
 	txt +="<li>&nbsp;</li>";
+	txt +="</ul>";
+	txt +="</div>";
+	txt +="<div class='col-lg-2 col-md-2 flechas-asesorias-der'>";
+	txt +="<div class='flechas'><a onclick='getGold();return false;' href='#''><h1>>></h1></a></div>";
+	txt +="</div></div>";
 
 				
 	$("#lista").html(txt);
@@ -53,6 +72,10 @@ function getPlatinum()
 {
 	var txt = "";
 	
+	txt +="<div id='lista'>";
+	txt +="<div class='col-lg-2 col-md-2 flechas-asesorias-izq'><div class='flechas'><a onclick='getGold();return false;' href='#''><h1><<</h1></a></div></div>";
+	txt +="<div class='col-lg-8 col-md-8 col-sm-12 col-xs-12'>";
+	txt +="<ul style='padding:0;text-align:center;font-family:noteworthy-light' class='good'>";
 	txt +="<li><h3>ASESORÍA PLATINUM</strong></h3>";
 	txt +="<li>Planteamiento de objetivos de Cliente</li>";
 	txt +="<li>Diagnóstico de Estilo</li>";
@@ -68,6 +91,13 @@ function getPlatinum()
 	txt +="<li>Básicos de Armario</li>";
 	txt +="<li>Cambio de look con especialista</li>";
 	txt +="<li>Personal Shopper</li>";
+	txt +="<li>Incluye: carpeta de diagnóstico y paleta de color de la estación correspondiente.</li>";
+	txt +="<li>&nbsp;</li>";
+	txt +="<li>&nbsp;</li>";
+	txt +="</ul>";
+	txt +="</div>";
+	txt +="<div class='col-lg-2 col-md-2 flechas-asesorias-der'>";
+	txt +="</div></div>";
 
 
 	$("#lista").html(txt);
